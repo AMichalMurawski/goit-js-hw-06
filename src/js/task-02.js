70b7d919a5c236e9744d163ba4668c8f49275e00
Michał Murawski <m.a.murawski@wp.pl>
2022-09-21 22:54:10 +0200

    Zadanie 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const liItem = [];
+for (let i = 0; i < ingredients.length; i += 1) {
+  liItem[i] = document.createElement("li");
+  liItem[i].textContent = ingredients[i];
+  liItem[i].classList.add("item");
+}
+
+const documentFragment = document.createDocumentFragment();
+liItem.forEach((element) => documentFragment.appendChild(element));
+
+const ulIngredients = document.querySelector("#ingredients");
+ulIngredients.append(documentFragment);
