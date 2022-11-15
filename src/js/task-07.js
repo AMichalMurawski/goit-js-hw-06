b291ceb7ea3f8b414705fa6cf6cd15f8f33451ae
Michał Murawski <109108678+AMichalMurawski@users.noreply.github.com>
2022-11-15 14:53:24 +0100

    Update task-07.js

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -3,7 +3,6 @@
   const spanText = document.querySelector("#text");
   
   inputSize.value = 16;
-  console.log(inputSize.value);
   inputSize.addEventListener("input", () => {
     spanText.style.fontSize = +inputSize.value + "px";
   });
